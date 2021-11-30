3373b6ca1b402db3c73227dc745b62d9dc925f7c
Ting Wei <endlesslove05230@gmail.com>
2021-11-30 15:34:32 +0800

    Add MathClass

--- GitHubSample/src/Gitapp/AppMain.java
+++ GitHubSample/src/Gitapp/AppMain.java
@@ -3,7 +3,15 @@ package Gitapp;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("2nd Version");
+		System.out.println("3rd Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
 	}
 
+}
+class Math {
+	int add(int a, int b) {
+		return a+b;
+	}
 }
